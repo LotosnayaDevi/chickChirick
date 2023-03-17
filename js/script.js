c279912803cdcc64a9de3aeb31ea1290b7064e1c
LotosnayaDevi <lotos.aya.devi@gmail.com>
2023-03-17 19:48:23 +0300

    next

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const addPreload = (elem) => {
+    elem.classList.add('preload')
+};
+
+const removePreload = (elem) => {
+    elem.classList.remove('preload');
+};
+
+const startSlider = (slider) => {
+}
+
+const initSlider = () => {
+    const slider = document.querySelector('.slider');
+    const sliderContainer = document.querySelector(".slider__container");
+    sliderContainer.style.display = 'none';
+
+    addPreload(slider)
+    window.addEventListener('load', () => {
+        sliderContainer.style.display = "";
+        removePreload(slider);
+        startSlider(slider);
+    });
+};
+
+window.addEventListener('DOMContentLoaded', initSlider);
